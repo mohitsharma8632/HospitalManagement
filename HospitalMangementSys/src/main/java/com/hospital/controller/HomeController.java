118fe1d85c69b80b487c73720e7e3d4e74082eb1
mohitsharma8632 <mohitsharma863252@gmail.com>
2020-06-28 11:54:06 +0530

    package fix

--- HospitalMangementSys/src/main/java/com/hospital/controller/HomeController.java
+++ HospitalMangementSys/src/main/java/com/hospital/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.hospital.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- HospitalMangementSys/src/main/java/com/hospital/controller/PatientController.java
+++ HospitalMangementSys/src/main/java/com/hospital/controller/PatientController.java
@@ -1,12 +1,11 @@
-package com.controller;
+package com.hospital.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.model.Patient;
-
+import com.hospital.model.*;
 @Controller
 public class PatientController {
 
