892a3269f01ea099ed9eea33a3858be912bdfc6e
mohitsharma8632 <mohitsharma863252@gmail.com>
2020-06-28 09:45:03 +0530

    hhh

--- HospitalMangementSys/src/main/java/com/hospital/controller/Acontroller.java
+++ HospitalMangementSys/src/main/java/com/hospital/controller/Acontroller.java
@@ -5,11 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hospital.model.Patient;
+
 @Controller
 public class Acontroller {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public static ModelAndView first() {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+		
+	}
+	@RequestMapping("home")
+	public static ModelAndView first(Patient p) {
 		ModelAndView mv=new ModelAndView();
 		mv.setViewName("index");
 		return mv;
